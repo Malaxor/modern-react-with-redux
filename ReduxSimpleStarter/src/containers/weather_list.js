4a51b898691d7fe53be935c34e57922a46c4fa34
Malaxor <nogobaduk@gmail.com>
2018-09-10 14:07:07 -0700

    maping over individual items in an array to construct the table body

--- ReduxSimpleStarter/src/containers/weather_list.js
+++ ReduxSimpleStarter/src/containers/weather_list.js
@@ -4,6 +4,23 @@ import { connect } from 'react-redux';
 
 class WeatherList extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.renderWeather = this.renderWeather.bind(this);
+    }
+
+    renderWeather(cityData) {
+
+        const name = cityData.city.name;
+        
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+            </tr>
+        );
+    }
+
     render() {
         return (
             <table className='table table-hover'>
@@ -16,6 +33,7 @@ class WeatherList extends Component {
                     </tr>
                 </thead>
                 <tbody>
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
