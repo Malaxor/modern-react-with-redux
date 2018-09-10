3809191aabade744e50e18bad4bef03a0a6b9106
Malaxor <nogobaduk@gmail.com>
2018-09-10 13:12:47 -0700

    new redux project

---
+++ ReduxSimpleStarter/src/actions/index.js
@@ -0,0 +1,18 @@
+
+import axios from 'axios';
+
+const API_KEY = '0f9c1491f939bc216355470522422e88';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
