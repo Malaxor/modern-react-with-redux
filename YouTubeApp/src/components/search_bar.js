ada8d20e96fa9c757a4e6b338b88023b33e18299
Malaxor <nogobaduk@gmail.com>
2019-01-23 18:29:10 -0700

    added videos project to directory

--- YouTubeApp/src/components/search_bar.js
+++ YouTubeApp/src/components/search_bar.js
@@ -18,7 +18,7 @@ class SearchBar extends Component {
                     value={this.state.term} 
                     onChange={e => {
                         this.setState({term: e.target.value });
-                        this.props.onSearchTermChange(this.state.term)
+                        this.props.onSearchTermChange(this.state.term);
                     }}
                 />
             </div>
